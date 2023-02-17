8df4cfaaa95c440c9220f136389c59017dffe7ef
S00236573 <S00236573@LAB-D2036-14>
2023-02-17 09:25:05 +0000

    Add project files.

---
+++ part1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * aoife hunt
+ * remake of the worksheet 3
+ * pushed to GitHub
+ */
+
+namespace part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
